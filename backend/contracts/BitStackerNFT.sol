274e0c7a83294d83595dbb761ed63f8e6e8e316b
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2022-08-26 10:48:18 +0500

    Updates in contract

--- backend/contracts/BitStackerNFT.sol
+++ backend/contracts/BitStackerNFT.sol
@@ -7,6 +7,18 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 // import "hardhat/console.sol";
 
 
+/*
+- Updates in the contract:
+    Add “bitcoin address” and “email address” inputs in the minting process
+    On transferring a token, its bitcoin address and email address will reset
+    Add functionality to update the bitcoin address and email address by the token owner
+
+- Updates in the front-end:
+    Users can request the withdrawal through signing a message with their wallet and we will send 
+    an encrypted message along with their tokenid, Ethereum address and bitcoin address to the admin.
+
+*/
+
 error SALE_IS_NOT_LIVE();
 error MINT_ONLY_VIP_TOKENS();
 error MINT_ONLY_NORMAL_TOKENS();
@@ -15,6 +27,7 @@ error NOT_ENOUGH_HATHRATE_AVAILABLE();
 error NULL_BALANCE();
 error UNABLE_TO_TRANSFER_FUNDS();
 error TOKEN_NOT_EXIST();
+error BOTH_CANT_BE_TRUE();
 
 contract BitStackerNFT is Ownable, ERC1155Supply {
 
@@ -38,6 +51,9 @@ contract BitStackerNFT is Ownable, ERC1155Supply {
     string public name = "BitStacker Tokens";
     string private baseURL = "https://ipfs.io/ipfs/QmXtQ3CdFaTMRFBAz36N47R8dhJ1REPKnxaGxaS47SxroA/";
 
+    mapping (address => string) public bitCoinAddress;
+    mapping (address => string) public emailAddress;
+
     enum Category {VIPBLACK, VIPBLUE, BLACK, BLUE}
 
     struct Tokentype {
@@ -52,7 +68,12 @@ contract BitStackerNFT is Ownable, ERC1155Supply {
 
     constructor() ERC1155(""){}
     
-    function mint(Category _category, uint256 _amount) payable public {
+    function mint(
+        Category _category, 
+        uint256 _amount,
+        string memory _bitcoinAddress,
+        string memory _emailAddress
+        ) payable public {
 
         if(!privateSale && !publicSale) revert SALE_IS_NOT_LIVE();
 
@@ -114,6 +135,9 @@ contract BitStackerNFT is Ownable, ERC1155Supply {
 
         }
 
+        bitCoinAddress[msg.sender] = _bitcoinAddress;
+        emailAddress[msg.sender] = _emailAddress;
+
     }
 
     function totalPresaleTerraHashesSold() public view returns(uint) {
@@ -135,9 +159,37 @@ contract BitStackerNFT is Ownable, ERC1155Supply {
         );
     }
 
+
+    function resetPersonalData(
+        string memory _bitcoinAddress,
+        string memory _emailAddress
+    ) public {
+        bitCoinAddress[msg.sender] = _bitcoinAddress;
+        emailAddress[msg.sender] = _emailAddress;
+    }
+
+    function balancesOf(address user) public view returns(
+        uint256 _vipBlack, uint256 _vipBlue, uint256 _black, uint256 _blue
+    ) {
+        _vipBlack = balanceOf(user, 0);
+        _vipBlue = balanceOf(user, 1);
+        _black = balanceOf(user, 2);
+        _blue = balanceOf(user, 3);
+    }
+
+    function hashesOf(address user) public view returns(
+        uint256 _vipBlackHash, uint256 _vipBlueHash, uint256 _blackHash, uint256 _blueHash
+    ) {
+        _vipBlackHash = (balanceOf(user, 0))*vipBlack.hashRate;
+        _vipBlueHash = (balanceOf(user, 1))*vipBlue.hashRate;
+        _blackHash = (balanceOf(user, 2))*black.hashRate;
+        _blueHash = (balanceOf(user, 3))*blue.hashRate;
+    }
+
     // Functions for administration
 
     function setSaleType(bool _privateSale, bool _publicSale) public onlyOwner {
+        if(_privateSale == true && _publicSale == true) revert BOTH_CANT_BE_TRUE();
         privateSale = _privateSale;
         publicSale = _publicSale;
     }
