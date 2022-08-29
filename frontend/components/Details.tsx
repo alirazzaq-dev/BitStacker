bd6df082f939812f4bb3a0da9c27d37813e7b7b1
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2022-08-29 16:37:44 +0500

    frontend changes

--- frontend/components/Details.tsx
+++ frontend/components/Details.tsx
@@ -10,10 +10,17 @@ import { BitStackerNFT } from "../types";
 // import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 // import { Doughnut } from "react-chartjs-2";
 
-import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Title } from 'chart.js';
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Title } from "chart.js";
 
-import { ArrowDown, Bitcoin, Chart, Corss, Etherum, Timer } from "../assets/icons";
+import {
+  ArrowDown,
+  Bitcoin,
+  Chart,
+  Corss,
+  Etherum,
+  Timer,
+} from "../assets/icons";
 import { ValidateBitcoinAddress, ValidateEmail } from "../utils/helpers";
 
 interface Balances {
@@ -36,9 +43,9 @@ interface Hashes {
   vipBlueHash: string;
 }
 
-
 const Details = () => {
-  const { account, library: provider } = useWeb3React<ethers.providers.JsonRpcProvider>();
+  const { account, library: provider } =
+    useWeb3React<ethers.providers.JsonRpcProvider>();
   // const [isDropDownOpen, setIsDropDownOpen] = useState(false);
 
   const [balances, setBalances] = useState<Balances>();
@@ -53,8 +60,6 @@ const Details = () => {
     bitCoinAddress: "",
     emailAddress: "",
   });
-
-
 
   const fetchContractDetails = async () => {
     if (provider && account) {
@@ -73,18 +78,18 @@ const Details = () => {
           _userHashes,
           _contactInfo,
         ]);
-        
+
         console.log("contactInfoX: ", contactInfoX);
 
         setContactInfo({
           bitCoinAddress: contactInfoX.bitCoinAddress,
-          emailAddress: contactInfoX.emailAddress
+          emailAddress: contactInfoX.emailAddress,
         });
 
         setUpdatedContactInfo({
           bitCoinAddress: contactInfoX.bitCoinAddress,
-          emailAddress: contactInfoX.emailAddress
-        })
+          emailAddress: contactInfoX.emailAddress,
+        });
 
         const balance = {
           black: userBalance._black.toString(),
@@ -92,7 +97,11 @@ const Details = () => {
           vipBlack: userBalance._vipBlack.toString(),
           vipBlue: userBalance._vipBlue.toString(),
         };
-        const totalBalance = Number(balance.black) + Number(balance.blue) + Number(balance.vipBlack) + Number(balance.vipBlue);
+        const totalBalance =
+          Number(balance.black) +
+          Number(balance.blue) +
+          Number(balance.vipBlack) +
+          Number(balance.vipBlue);
         const totalBlack = Number(balance.black) + Number(balance.vipBlack);
         const totalBlue = Number(balance.blue) + Number(balance.vipBlue);
 
@@ -102,28 +111,39 @@ const Details = () => {
           vipBlackHash: userHashes._vipBlackHash.toString(),
           vipBlueHash: userHashes._vipBlueHash.toString(),
         };
-        const totalHashes = Number(hashes.blackHash) + Number(hashes.blueHash) + Number(hashes.vipBlackHash) + Number(hashes.vipBlueHash);
-        const totalBlackHases = Number(hashes.blackHash) + Number(hashes.vipBlackHash);
-        const totalBlueHases = Number(hashes.blueHash) + Number(hashes.vipBlueHash);
-      
+        const totalHashes =
+          Number(hashes.blackHash) +
+          Number(hashes.blueHash) +
+          Number(hashes.vipBlackHash) +
+          Number(hashes.vipBlueHash);
+        const totalBlackHases =
+          Number(hashes.blackHash) + Number(hashes.vipBlackHash);
+        const totalBlueHases =
+          Number(hashes.blueHash) + Number(hashes.vipBlueHash);
+
         setBalances({ total: totalBalance, totalBlack, totalBlue, ...balance });
-        setHashes({ total: totalHashes, totalBlackHases, totalBlueHases, ...hashes });
+        setHashes({
+          total: totalHashes,
+          totalBlackHases,
+          totalBlueHases,
+          ...hashes,
+        });
       } catch (e) {
         console.error(e);
       }
     }
   };
 
-  const handleUpdatedContactInfo = (val: string, type: "bitcoin"|"email") => {
-    if(type === "bitcoin"){
-      setUpdatedContactInfo((e) => ({...e, bitCoinAddress: val}));
+  const handleUpdatedContactInfo = (val: string, type: "bitcoin" | "email") => {
+    if (type === "bitcoin") {
+      setUpdatedContactInfo((e) => ({ ...e, bitCoinAddress: val }));
+    } else {
+      setUpdatedContactInfo((e) => ({ ...e, emailAddress: val }));
     }
-    else {
-      setUpdatedContactInfo((e) => ({...e, emailAddress: val}));
-    }
-  }
+  };
 
-  const submitUpdatedContactInfo = async () => {   
+  const submitUpdatedContactInfo = async (e) => {
+    e.preventDefault();
     // const validEmail = ValidateEmail(updatedContactInfo.emailAddress);
     // if (!validEmail) {
     //   alert("Invalid email address");
@@ -143,13 +163,13 @@ const Details = () => {
     //       abis.BitStackerNFT,
     //       signer
     //     ) as BitStackerNFT;
-  
+
     //     const tx = await contract.resetContactInfo({
     //       bitCoinAddress: updatedContactInfo.bitCoinAddress,
     //       emailAddress: updatedContactInfo.emailAddress,
     //     });
     //     await tx.wait(1);
-  
+
     //     setContactInfo({
     //       bitCoinAddress: updatedContactInfo.bitCoinAddress,
     //       emailAddress: updatedContactInfo.emailAddress
@@ -161,42 +181,43 @@ const Details = () => {
     //     console.error(e);
     //   }
     // }
-
-
   };
 
   ChartJS.register(ArcElement, Title);
 
   enum DONUT {
     NFTS,
-    HASHES
+    HASHES,
   }
-  const [donut, setDonut] = useState(DONUT.NFTS)
+  const [donut, setDonut] = useState(DONUT.NFTS);
 
   const dataNFTs = {
     labels: ["Black NFts", "Blue NFTs"],
-    datasets: [{
-      label: 'Dataset',
-      data: [balances?.totalBlack, balances?.totalBlue],
-      backgroundColor: ["#171717", "#215FFF"],
-      borderColor: ["#363636", "#215FFF"],
-      hoverOffset: 4,
-      cutout: 120
-    }]
+    datasets: [
+      {
+        label: "Dataset",
+        data: [balances?.totalBlack, balances?.totalBlue],
+        backgroundColor: ["#171717", "#215FFF"],
+        borderColor: ["#363636", "#215FFF"],
+        hoverOffset: 4,
+        cutout: 120,
+      },
+    ],
   };
 
   const dataHashes = {
     labels: ["Black Hashes", "Blue Hashes"],
-    datasets: [{
-      label: 'Dataset',
-      data: [hashes?.totalBlackHases, hashes?.totalBlueHases],
-      backgroundColor: ["#171717", "#215FFF"],
-      borderColor: ["#363636", "#215FFF"],
-      cutout: 100
-    }]
+    datasets: [
+      {
+        label: "Dataset",
+        data: [hashes?.totalBlackHases, hashes?.totalBlueHases],
+        backgroundColor: ["#171717", "#215FFF"],
+        borderColor: ["#363636", "#215FFF"],
+        cutout: 100,
+      },
+    ],
   };
 
-  
   useEffect(() => {
     fetchContractDetails();
   }, [provider]);
@@ -321,7 +342,6 @@ const Details = () => {
       </div>
 
       <div className="bg-[#121212] flex flex-col justify-between rounded-3xl px-5 py-3 max-w-[442px] w-full ">
-        
         <div className="flex items-center justify-between">
           <h1 className="font-semibold text-xl">
             <span className="text-[#F4931E]">NFT</span> Portfolio
@@ -329,83 +349,81 @@ const Details = () => {
 
           <select
             className="bg-[#000000] cursor-pointer"
-            onChange={(e) => setDonut(Number(e.target.value))}>
-            <option
-              value={DONUT.NFTS}>
-              NFTs
-            </option>
-            <option value={DONUT.HASHES}>
-              Hashes
-            </option>
-
+            onChange={(e) => setDonut(Number(e.target.value))}
+          >
+            <option value={DONUT.NFTS}>NFTs</option>
+            <option value={DONUT.HASHES}>Hashes</option>
           </select>
-
         </div>
-
-
-
-        <div className=" w-[270px] mx-auto my-4 flex items-center justify-between">
+        <div className=" w-[270px] mx-auto my-4 flex items-center justify-between"></div>
+        <div className="relative">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2">
+            {donut === DONUT.NFTS
+              ? ` ${balances?.total} Tokens`
+              : `${hashes?.total} Terrahash`}
+          </div>
+          <Doughnut
+            width={250}
+            height={250}
+            data={donut === DONUT.NFTS ? dataNFTs : dataHashes}
+            // data={data}
+          />
         </div>
-
-
-            {donut === DONUT.NFTS ? `${balances?.total} Tokens` : `${hashes?.total} Terrahash`}
-            <Doughnut width={250} height={250} data={donut === DONUT.NFTS ? dataNFTs : dataHashes} />
-
       </div>
 
-
-
       {/* showModalUpdate */}
-      {
-        showModalUpdate && (
-          <div className="fixed top-0 left-0 bg-[#121212] bg-opacity-40 w-full h-full flex items-center justify-center ">
-            <div className="bg-[#121212] max-w-[687px] mx-auto border w-full border-[#F7931B] border-opacity-40 rounded-3xl pt-5 pb-10 px-8 box-shadow">
-              
-              <div className="flex items-center justify-between">
-                <h1 className="font-bold text-xl">
-                  Update <span className="text-[#F7931B]">Personal Info</span>
-                </h1>
-                <div
-                  onClick={() => { setShowModalUpdate(false) }}
-                  className="bg-[#FF5B5B] cursor-pointer bg-opacity-5 rounded-full w-10 h-10 flex items-center justify-center"
-                >
-                  <Corss />
-                </div>
+      {showModalUpdate && (
+        <div className="fixed top-0 left-0 bg-[#121212] bg-opacity-40 w-full h-full flex items-center justify-center ">
+          <div className="bg-[#121212] max-w-[687px] mx-auto border w-full border-[#F7931B] border-opacity-40 rounded-3xl pt-5 pb-10 px-8 box-shadow">
+            <div className="flex items-center justify-between">
+              <h1 className="font-bold text-xl">
+                Update <span className="text-[#F7931B]">Personal Info</span>
+              </h1>
+              <div
+                onClick={() => {
+                  setShowModalUpdate(false);
+                }}
+                className="bg-[#FF5B5B] cursor-pointer bg-opacity-5 rounded-full w-10 h-10 flex items-center justify-center"
+              >
+                <Corss />
               </div>
-
-              <form className="flex flex-col items-center mt-5 space-y-5">
-                <div className="w-full relative border border-[#595959] rounded-[50px] py-4 px-6 my-1">
-                  <input
-                    value={updatedContactInfo.bitCoinAddress}
-                    onChange={(e) => handleUpdatedContactInfo(e.target.value, "bitcoin")}
-                    className="w-full bg-transparent outline-none font-medium text-lg"
-                  />
-                  <p className="absolute -top-4 left-7  font-light text-base px-1 bg-[#121212]">
-                    <span className="opacity-50">New Bitcoin Address</span>
-                  </p>
-                </div>
-                <div className="w-full relative border border-[#595959] rounded-[50px] py-4 px-6 my-1">
-                  <input
-                    value={updatedContactInfo.emailAddress}
-                    onChange={(e) => handleUpdatedContactInfo(e.target.value, "email")}
-                    className="w-full bg-transparent outline-none font-medium text-lg"
-                  />
-                  <p className="absolute -top-4 left-7  font-light text-base px-1 bg-[#121212]">
-                    <span className="opacity-50">New Email Address</span>
-                  </p>
-                </div>
-                <button
-                  onClick={submitUpdatedContactInfo}
-                  className="bg-[#F7931B] py-4 mt-8 font-bold text-2xl px-20 rounded-[50px]"
-                >
-                  Update
-                </button>
-              </form>
-
             </div>
+
+            <form className="flex flex-col items-center mt-5 space-y-5">
+              <div className="w-full relative border border-[#595959] rounded-[50px] py-4 px-6 my-1">
+                <input
+                  value={updatedContactInfo.bitCoinAddress}
+                  onChange={(e) =>
+                    handleUpdatedContactInfo(e.target.value, "bitcoin")
+                  }
+                  className="w-full bg-transparent outline-none font-medium text-lg"
+                />
+                <p className="absolute -top-4 left-7  font-light text-base px-1 bg-[#121212]">
+                  <span className="opacity-50">New Bitcoin Address</span>
+                </p>
+              </div>
+              <div className="w-full relative border border-[#595959] rounded-[50px] py-4 px-6 my-1">
+                <input
+                  value={updatedContactInfo.emailAddress}
+                  onChange={(e) =>
+                    handleUpdatedContactInfo(e.target.value, "email")
+                  }
+                  className="w-full bg-transparent outline-none font-medium text-lg"
+                />
+                <p className="absolute -top-4 left-7  font-light text-base px-1 bg-[#121212]">
+                  <span className="opacity-50">New Email Address</span>
+                </p>
+              </div>
+              <button
+                onClick={submitUpdatedContactInfo}
+                className="bg-[#F7931B] py-4 mt-8 font-bold text-2xl px-20 rounded-[50px]"
+              >
+                Update
+              </button>
+            </form>
           </div>
-        )
-      }
+        </div>
+      )}
 
       {/* Withdraw */}
       {showModal && (
@@ -472,44 +490,34 @@ const Details = () => {
           </div>
         </div>
       )}
-
     </div>
   );
 };
 
 export default Details;
 
-
-          {/* <div
-            onClick={() => {
-              setIsDropDownOpen(!isDropDownOpen);
-            }}
-            className="bg-[#000000] relative rounded-full flex items-center justify-center font-lighter text-xs px-4 py-2  cursor-pointer"
-          >
-            <span>Monthly</span>
-            <ArrowDown />
-
-            <div
-              className={`${
-                isDropDownOpen ? " block " : " hidden "
-              } absolute top-9 left-0 bg-[#000000] w-full p-2 rounded-lg space-y-1`}
-            >
-              <h5
-                onClick={() => {
-                  setIsDropDownOpen(!isDropDownOpen);
-                }}
-                className="text-center duration-300 hover:bg-[#121212] p-1 rounded-lg"
-              >
-                Weekly
-              </h5>
-              <h5
-                onClick={() => {
-                  setIsDropDownOpen(!isDropDownOpen);
-                }}
-                className="text-center duration-300 hover:bg-[#121212] p-1 rounded-lg"
-              >
-                Yearly
-              </h5>
-            </div>
-
-          </div> */}
+export const data = {
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
